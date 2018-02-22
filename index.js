f24404314a4b981267447b2215ee7faae528b83e
Mitchell Gates <ms.fragala@gmail.com>
2018-02-22 07:26:01 -0500

    Rename to dhas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export default function deephas(o, k) {
+export default function dhas(o, k) {
   k.split && (k=k.split('.'));
   for(var i = 0; i < k.length; i++){
-    if (o && Object.prototype.hasOwnProperty.call(o, k[i])) o = o[k[i]];
+    if (o != null && Object.prototype.hasOwnProperty.call(o, k[i])) o = o[k[i]];
     else return false;
   }
   return true;
--- test.js
+++ test.js
@@ -1,50 +1,50 @@
-const has = require('./dist/deephas');
+const dhas = require('./dist/deepdhas');
 
 test('should consider `undefined` a value', () => {
   const inputA = { a: undefined };
   const inputB = { a: { b: undefined } };
-  expect(has(inputA, 'a')).toBe(true);
-  expect(has(inputB, 'a.b')).toBe(true);
+  expect(dhas(inputA, 'a')).toBe(true);
+  expect(dhas(inputB, 'a.b')).toBe(true);
 });
 
 test('should consider `false` a value', () => {
   const inputA = { a: false };
   const inputB = { a: { b: false } };
-  expect(has(inputA, 'a')).toBe(true);
-  expect(has(inputB, 'a.b')).toBe(true);
+  expect(dhas(inputA, 'a')).toBe(true);
+  expect(dhas(inputB, 'a.b')).toBe(true);
 });
 
 test('should consider `null` a value', () => {
   const inputA = { a: null };
   const inputB = { a: { b: null } };
-  expect(has(inputA, 'a')).toBe(true);
-  expect(has(inputB, 'a.b')).toBe(true);
+  expect(dhas(inputA, 'a')).toBe(true);
+  expect(dhas(inputB, 'a.b')).toBe(true);
 });
 
 test('should accept array as first argument', () => {
-  expect(has([], '0')).toBe(false);
-  expect(has([0], '0')).toBe(true);
+  expect(dhas([], '0')).toBe(false);
+  expect(dhas([0], '0')).toBe(true);
 });
 
 test('should accept object as first argument', () => {
   const inputA = {};
   const inputB = { a: '' };
   const inputC = { a: { b: true } };
-  expect(has(inputA, 'a')).toBe(false);
-  expect(has(inputB, 'a')).toBe(true);
-  expect(has(inputC, 'a.b')).toBe(true);
+  expect(dhas(inputA, 'a')).toBe(false);
+  expect(dhas(inputB, 'a')).toBe(true);
+  expect(dhas(inputC, 'a.b')).toBe(true);
 });
 
 test('should accept dot-delineated string as second argument', () => {
   const inputA = { a: '' };
   const inputB = { a: { b: true } };
-  expect(has(inputA, 'a')).toBe(true);
-  expect(has(inputB, 'a.b')).toBe(true);
+  expect(dhas(inputA, 'a')).toBe(true);
+  expect(dhas(inputB, 'a.b')).toBe(true);
 });
 
 test('should accept array as second argument', () => {
   const inputA = { a: '' };
   const inputB = { a: { b: true } };
-  expect(has(inputA, ['a'])).toBe(true);
-  expect(has(inputB, ['a', 'b'])).toBe(true);
+  expect(dhas(inputA, ['a'])).toBe(true);
+  expect(dhas(inputB, ['a', 'b'])).toBe(true);
 });
