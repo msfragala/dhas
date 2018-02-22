12360f6472d0612e6fa4359d17c61c21de049ca6
Mitchell Gates <ms.fragala@gmail.com>
2018-02-22 07:40:16 -0500

    Fix tests

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const dhas = require('./dist/deepdhas');
+const dhas = require('./dist/dhas');
 
 test('should consider `undefined` a value', () => {
   const inputA = { a: undefined };
@@ -21,30 +21,34 @@ test('should consider `null` a value', () => {
   expect(dhas(inputB, 'a.b')).toBe(true);
 });
 
-test('should accept array as first argument', () => {
-  expect(dhas([], '0')).toBe(false);
-  expect(dhas([0], '0')).toBe(true);
+describe('first parameter', () => {
+  test('should accept array', () => {
+    expect(dhas([], '0')).toBe(false);
+    expect(dhas([0], '0')).toBe(true);
+  });
+
+  test('should accept object', () => {
+    const inputA = {};
+    const inputB = { a: '' };
+    const inputC = { a: { b: true } };
+    expect(dhas(inputA, 'a')).toBe(false);
+    expect(dhas(inputB, 'a')).toBe(true);
+    expect(dhas(inputC, 'a.b')).toBe(true);
+  });
 });
 
-test('should accept object as first argument', () => {
-  const inputA = {};
-  const inputB = { a: '' };
-  const inputC = { a: { b: true } };
-  expect(dhas(inputA, 'a')).toBe(false);
-  expect(dhas(inputB, 'a')).toBe(true);
-  expect(dhas(inputC, 'a.b')).toBe(true);
-});
+describe('second parameter', () => {
+  test('should accept string (delineated by dots)', () => {
+    const inputA = { a: '' };
+    const inputB = { a: { b: true } };
+    expect(dhas(inputA, 'a')).toBe(true);
+    expect(dhas(inputB, 'a.b')).toBe(true);
+  });
 
-test('should accept dot-delineated string as second argument', () => {
-  const inputA = { a: '' };
-  const inputB = { a: { b: true } };
-  expect(dhas(inputA, 'a')).toBe(true);
-  expect(dhas(inputB, 'a.b')).toBe(true);
-});
-
-test('should accept array as second argument', () => {
-  const inputA = { a: '' };
-  const inputB = { a: { b: true } };
-  expect(dhas(inputA, ['a'])).toBe(true);
-  expect(dhas(inputB, ['a', 'b'])).toBe(true);
+  test('should accept array', () => {
+    const inputA = { a: '' };
+    const inputB = { a: { b: true } };
+    expect(dhas(inputA, ['a'])).toBe(true);
+    expect(dhas(inputB, ['a', 'b'])).toBe(true);
+  });
 });
